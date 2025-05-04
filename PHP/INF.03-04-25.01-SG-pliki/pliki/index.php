a47ce15df012c6fadaf2862b8a0f898b6b058c29
SmoczaSkala <99537042+SmoczaSkala@users.noreply.github.com>
2025-05-04 22:19:53 +0200

    egzamin poczatek

---
+++ PHP/INF.03-04-25.01-SG-pliki/pliki/index.php
@@ -0,0 +1,24 @@
+<?php
+$conn = new mysql('localhost', 'root', '', 'obuwie')
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Obuwie</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Obuwie męskie</h1>
+    </header>
+    <main></main>
+    <footer><p>Autor strony: PESEL</p></footer>
+  </body>
+</html>
+
+<?php
+$conn -> close()
+?>
---
+++ PHP/INF.03-04-25.01-SG-pliki/pliki/zamow.php
@@ -0,0 +1,33 @@
+<?php
+$conn = new mysql('localhost', 'root', '', 'obuwie')
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Obuwie</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Obuwie męskie</h1>
+    </header>
+    <main>
+
+<?php
+
+
+
+?>
+
+    </main>
+    <footer><p>Autor strony: PESEL</p></footer>
+  </body>
+</html>
+
+
+<?php
+$conn -> close()
+?>
